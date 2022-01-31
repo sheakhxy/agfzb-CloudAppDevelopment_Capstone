c2288c3d0158f9610507261f017cfbabd047ed5c
sheakhxy <sn.khan@ymail.com>
2022-01-31 01:31:34 +0000

    changes made from theia lab environment in the final project

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,15 +10,15 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path('about/', views.about_request, name='about'),
     # path for contact us view
-
+    path('contact/', views.contact_request, name='contact'),
     # path for registration
-
+    path('registration/', views.registration_request, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
-
+    path('logout/', views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost","snk-dealer-reiew-203.au-syd.cf.appdomain.cloud"]
 
 
 # Application definition
